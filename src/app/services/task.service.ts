f3da2fe0ffb013041aebf274dbfdac5f98529022
Durgesh Chaudhary <48durgesh.kumar@gmail.com>
2023-07-29 22:15:17 +0530

    dev api url replaced with server api

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -12,7 +12,9 @@ const httpOptions = {
   providedIn: 'root',
 })
 export class TaskService {
-  private apiUrl = 'http://localhost:5000/tasks';
+  // private apiUrl = 'http://localhost:5000/tasks';
+  private apiUrl =
+    'https://my-json-server.typicode.com/yodkwtf/ng-checklist-angular/tasks';
 
   constructor(private http: HttpClient) {}
 
